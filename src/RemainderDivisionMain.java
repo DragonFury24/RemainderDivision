92a196ee60a175970044fa756dfc41dd4b370506
Nguyen <1383504@fcps.edu>
2017-09-12 09:25:01 -0400

    Add error checking

--- src/RemainderDivisionMain.java
+++ src/RemainderDivisionMain.java
@@ -1,28 +1,30 @@
 import java.util.*;
 
 public class RemainderDivisionMain {
-   public static void main (String[] args) {
-      Scanner keyboard = new Scanner(System.in);
-      System.out.println("Please input the first number");
-      double num0 = dInput(keyboard);
-      System.out.println("Please input the second number");
-      double num1 = dInput(keyboard);
-      System.out.println((int)num0 + " divided by " + (int)num1 + " is " + (int)(num0 / num1) + " with a remainder of " + (int)(num0 % num1));
-   
-   }
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Please input the first number");
+        double num0 = dInput(keyboard);
+        System.out.println("Please input the second number");
+        double num1 = dInput(keyboard);
+        if (num0 > num1)
+        System.out.println((int) num0 + " divided by " + (int) num1 + " is " + (int) (num0 / num1) + " with a remainder of " + (int) (num0 % num1));
+        else System.out.println((int) num1 + " divided by " + (int) num0 + " is " + (int) (num1 / num0) + " with a remainder of " + (int) (num1 % num0));
 
-   private static double dInput(Scanner keyboard) { //checks if input is a double. If not, then will give error and force user to retry
-      double dOutput = 0;
-      boolean noInput = true;
-      while (noInput) {
-         try {
-            dOutput = keyboard.nextDouble();
-            noInput = false;
-         } catch (InputMismatchException I) {
-            System.out.println("You did not input a number. Please try again.");
-            keyboard.next();
-         }
-      }
-      return dOutput;
-   }
+    }
+
+    private static double dInput(Scanner keyboard) { //checks if input is a double. If not, then will give error and force user to retry
+        double dOutput = 0;
+        boolean noInput = true;
+        while (noInput) {
+            try {
+                dOutput = keyboard.nextDouble();
+                noInput = false;
+            } catch (InputMismatchException I) {
+                System.out.println("You did not input a number. Please try again.");
+                keyboard.next();
+            }
+        }
+        return dOutput;
+    }
 }
